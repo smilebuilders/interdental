dcb96b304933049a1685c0d5a302d9e6c1956f82
Arturo Macias <sb.marketingchief@gmail.com>
2019-02-08 13:57:32 -0800

    activar soft delete en tratamientos del paciente

--- app/PatientTreatment.php
+++ app/PatientTreatment.php
@@ -2,14 +2,18 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class PatientTreatment extends Model
 {
+    use SoftDeletes;
+    
     public function patient()
     {
       return $this->belongsTo('App\Patient');
     }
+    
     public function claim()
     {
       return $this->belongsTo('App\Claim');
@@ -22,4 +26,6 @@ class PatientTreatment extends Model
 
     protected $fillable = ['number', 'sc', 'df', 'code', 'description', 'date', 'images', 'patient_id'];
     protected $table = 'patients_treatments';
+
+    protected $dates = ['deleted_at'];
 }
