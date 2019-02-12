0602223e789ab6d2ad43020b4e31a91dc5e8d4aa
Arturo Macias <sb.marketingchief@gmail.com>
2019-02-11 18:29:47 -0800

    se arreglo la tabla de reporte

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 @section('content')
-  <table class="table table-striped table-sm">
+  <table class="table table-striped table-sm" style="font-size: 12px;" data-show-print="true">
     <thead>
       <tr>
         <th>Claim</th>
@@ -21,14 +21,26 @@
       @foreach ($claims as $claim)
         <tr>
           <th>{{ $claim->code }}</th>
-          <th>{{ $claim->code }}</th>
-          <th>{{ $claim->code }}</th>
+          <th>{{ $claim->created_at->format('d M Y') }}</th>
+          <th>Claim</th>
           <th>{{ $claim->status }}</th>
-          <th>{{ $claim->patient->first_name }}</th>
+          <th>{{ $claim->patient->first_name . ', ' . $claim->patient->last_name}}</th>
           <th>{{ $claim->patient->nss }}</th>
           <th>{{ $claim->patient->policy_code }}</th>
           <th>{{ $claim->patient->birth_date }}</th>
-
+          <th>{{ $claim->created_at->format('d M Y') }}</th>
+          <th>{{ $claim->patient->insurance->name }}</th>
+          <th>
+            {{-- {{ $claim->treatments }} --}}
+            @php
+                $treatments = '';
+              foreach($claim->treatments as $treatment) {
+                $treatments = $treatments . $treatment->description . ', ';
+              }
+              echo $treatments;
+              @endphp 
+          </th>
+          <th>{{ $claim->created_at->format('d M Y') }}</th>
         </tr>
       @endforeach
     </tbody>
